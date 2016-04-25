ac440694cd2ae47227134b3f22a41a7b5baa5376
Pedro Morelatto <pedro.morelatto@gmail.com>
2016-04-25 00:02:42 -0300

    Fifth Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ matrix_test = [["I", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "O"]]
 compass = {"north": "south", "south": "north", "east": "west", "west": "east"}
 
 
-def file_to_matrix(filename, total_lines=7, total_columns=7):
+def file_to_matrix(filename, total_lines=7, total_columns=8):
     m = [[0 for _ in range(total_columns)] for _ in range(total_lines)]
     line, column = 0, 0
 
@@ -30,6 +30,10 @@ def print_matrix():
     print()
 
 
+def initial_move():
+    walk([1, 2])
+
+
 def walk(new_pos):
     old_pos = get_runner_position()
     matrix[old_pos[0]][old_pos[1]], matrix[new_pos[0]][new_pos[1]] = 0, matrix[old_pos[0]][old_pos[1]]
@@ -44,25 +48,30 @@ def get_runner_position(runner="I"):
     return runner_position
 
 
-def run(going_to, coming_from):
+def run(direction):
     print_matrix()
 
     runner_position = get_runner_position()
     walls = get_options(runner_position, walls=1)
     paths = get_options(runner_position, walls=0)
-    paths.pop(coming_from, None)
+    # paths.pop(compass[direction], None)
+
+    print("Going To -", direction)
+    print("Coming From -", compass[direction])
 
     print("Walls -", walls)
     print("Paths -", paths)
 
-    print("Going To -", going_to)
-    print("Coming From -", coming_from)
-
     if len(walls) == 3:
-        run(compass[going_to], going_to)
-    elif (4 - len(walls)) == 2:
-        walk(paths[going_to])
-        run(compass[going_to], going_to)
+        run(compass[direction])
+    if len(paths) == 1:
+        walk(list(dict.values(paths))[0])
+        run(direction)
+    # if len(paths) == 3:
+    #     run(compass[going_to], going_to)
+    # elif (4 - len(paths)) == 2:
+    #     walk(paths[going_to])
+    #     run(compass[going_to], going_to)
 
 
 def get_options(position, walls):
@@ -78,13 +87,8 @@ def main():
     global matrix
     matrix = file_to_matrix("maze.txt")
 
-    # print(get_runner_position())
-    #
-    # walk([1, 1], [1, 2])
-    # print_matrix()
-    #
-    # print(get_runner_position())
+    initial_move()
 
-    run("east", compass["east"])
+    run("east")
 
 main()
